4add0a9a55722db3cbff6c53bc055849eaea4af8
matheus-miranda <matheus.sm86@gmail.com>
2021-10-22 10:29:27 -0300

    Cria teste para mesmo usuario dar lances seguidos

--- app/src/test/java/br/com/alura/leilao/api/EnviadorDeLanceTest.java
+++ app/src/test/java/br/com/alura/leilao/api/EnviadorDeLanceTest.java
@@ -1,11 +1,10 @@
 package br.com.alura.leilao.api;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-
-import android.content.Context;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +12,8 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.api.retrofit.client.RespostaListener;
+import br.com.alura.leilao.exception.LanceSeguidoDoMesmoUsuarioException;
 import br.com.alura.leilao.exception.UsuarioJaDeuCincoLancesException;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
@@ -22,16 +23,20 @@ import br.com.alura.leilao.ui.dialog.AvisoDialogManager;
 @RunWith(MockitoJUnitRunner.class)
 public class EnviadorDeLanceTest {
 
+    private EnviadorDeLance enviadorDeLance;
+
     @Mock
     private LeilaoWebClient client;
     @Mock
     private EnviadorDeLance.LanceProcessadoListener listener;
     @Mock
     private AvisoDialogManager aviso;
+    @Mock
+    private Leilao leilao;
 
     @Test
     public void deve_MostrarMensagemDeFalha_QuandoLanceForMenorQueUltimoLance() {
-        EnviadorDeLance enviadorDeLance = new EnviadorDeLance(client, listener, aviso);
+        enviadorDeLance = new EnviadorDeLance(client, listener, aviso);
         Leilao leilao = new Leilao("Console");
         Lance joao = new Lance(new Usuario("Joao"), 200.0);
 
@@ -43,14 +48,35 @@ public class EnviadorDeLanceTest {
 
     @Test
     public void deve_MostrarMensagemDeFalha_QuandoUsuarioDerMaisDeCincoLances() {
-        EnviadorDeLance enviadorDeLance = new EnviadorDeLance(client, listener, aviso);
-        Leilao leilao = mock(Leilao.class);
+        enviadorDeLance = new EnviadorDeLance(client, listener, aviso);
         doThrow(UsuarioJaDeuCincoLancesException.class)
                 .when(leilao).propoe(any(Lance.class));
 
         enviadorDeLance.envia(leilao, new Lance(new Usuario("Joao"), 200.0));
 
         verify(aviso).mostraAvisoUsuarioJaDeuCincoLances();
+    }
+
+    @Test
+    public void deve_MostrarMensagemDeFalha_QuandoOUsuarioDoUltimoLanceDerNovoLance() {
+        enviadorDeLance = new EnviadorDeLance(client, listener, aviso);
+        doThrow(LanceSeguidoDoMesmoUsuarioException.class).when(leilao).propoe(any(Lance.class));
+
+        enviadorDeLance.envia(leilao, new Lance(new Usuario("Joao"), 200.0));
+
+        verify(aviso).mostraAvisoLanceSeguidoDoMesmoUsuario();
+        // Verifica que após chamar a exceção o propoe nao é mais chamado
+        verify(client, never()).propoe(any(Lance.class), anyLong(), any(RespostaListener.class));
+    }
+
+    @Test
+    public void deve_MostraMensagemDeFalha_QuandoFalharEnvioDeLanceParaAPI() {
+
+    }
+
+    @Test
+    public void deve_NotificarLanceProcessado_QuandoEnviarLanceParaAPIComSucesso() {
+
     }
 
 }
